dee83753024f7d5d5502b0394ddcfd20b06f3508
allennaur <allennaur@163.com>
2018-03-23 13:57:27 +0800

    allennaur.cn/labels 页面初次建构

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
+import {
+  DatePicker,
+  Input,
+  Select,
+} from 'antd';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
+const Option = Select.Option;
+const Search = Input.Search;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectValues: 'baidu',        //选择框选中值
+    };
+  }
+
+  selectBefore (){
+    return(
+      <Select defaultValue={this.state.selectValues} color="white" style={{ width: 100}} className="searchBefore" onSelect={(value) => {this.setState({selectValues: value})}}>
+        <Option value="baidu">百度一下</Option>
+        <Option value="google">Google</Option>
+      </Select>
+    )
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          Please visit https://www.allennaur.cn/labels
-        </p>
+        <div className="Top-search-area">
+          <div className="search-input">
+            <Search size="large" className="input-area" addonBefore={this.selectBefore()} onSearch={(info) => this.goToSearch(info)} placeholder="Search..." />
+          </div>
+        </div>
+        <div className="Main-area"></div>
+        <div className="Copy-area"></div>
       </div>
     );
+  }
+
+  goToSearch(info) {
+    let url = '';
+    switch(this.state.selectValues){
+      case 'baidu':
+                  url = 'https://www.baidu.com/s?wd=';
+                  break;
+      case 'google':
+                  url = 'https://www.google.com/search?q=';
+                  break;
+      default: break;
+    }
+    window.open(url+info);
   }
 }
 
